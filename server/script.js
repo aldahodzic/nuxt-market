84382f09ccf34032485d2ae948d30fe20b9c03f0
aldahodzic <aldahodzic844@gmail.com>
2020-06-25 02:27:16 +0300

    update: rest api update

--- server/script.js
+++ server/script.js
@@ -1,15 +1,50 @@
 const express = require('express');
-const path = require('path'); // модуль для парсинга пути
 const ngrok = require('ngrok');
 const app = express();
 
-app.get('/api', (req, res) => {
-    res.send('API is running');
+const products = [
+    {
+        category: 'Kugoo',
+        products: [
+            {
+                name: 'Kugoo X1',
+                description: 'Lorem ipsum dolor sit amet',
+                price: '30000'
+            }
+        ]
+    },
+    {
+        category: 'White-Siberia',
+        products: [
+            {
+                name: 'Thunder',
+                description: 'Lorem ipsum dolor sit amet',
+                price: '51900'
+            }
+        ]
+    }
+]
+
+app.get('/all', (req, res) => {
+    if(req.query.category) {
+        let categoryProducts = products.find(item => item.category.toLowerCase() === req.query.category.toLowerCase());
+        if(categoryProducts) {
+            res.send(categoryProducts.products[0])
+        } else {
+            res.send('404')
+        }
+    } else {
+        res.send(products)
+    }
 });
 
 app.listen(1337, function(){
     (async function() {
-        const connect = await ngrok.connect(1337);
-        console.log(`connected on 1337 and ${connect}`);
+        try {
+            const connect = await ngrok.connect(1337);
+            console.log(`connected on 1337 and ${connect}`);
+        } catch(e) {
+            throw e
+        }
     })()
 });
