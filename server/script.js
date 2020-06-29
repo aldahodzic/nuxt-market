d52bf6ed961fe0911497ddac5ee5a388c852abd0
aldahodzic <aldahodzic844@gmail.com>
2020-06-29 12:54:15 +0300

    remove static products

--- server/script.js
+++ server/script.js
@@ -14,37 +14,6 @@ const productRoutes = require('./routes/products');
 //middleware
 app.use('/', productRoutes);
 
-const products = [
-    {
-        category: "Kugoo",
-        products: [
-            {
-                name: "Kugoo X1",
-                img: "https://static.tildacdn.com/tild3039-6437-4330-b733-643030396430/2020-04-04_120022_.jpg",
-                description: "Lorem ipsum dolor sit amet",
-                price: "30000"
-            },
-            {
-                name: "Kugoo M4 Pro",
-                img: "https://static.tildacdn.com/tild3333-6130-4966-b734-356434616335/DSC_7652__.jpg",
-                description: "Lorem ipsum",
-                price: "34000"
-            }
-        ]
-    },
-    {
-        category: "White-Siberia",
-        products: [
-            {
-                name: "Thunder",
-                img: "https://images.by.prom.st/182792971_w640_h640_182792971.jpg",
-                description: "Lorem ipsum dolor sit amet",
-                price: "51900"
-            }
-        ]
-    }
-]
-
 // connect to DB
 mongoose.connect(
     process.env.DB_CONNECT,
